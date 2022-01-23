7c2a0ef5df663abe209ee9ae7fe1e2bccf0dc0e1
Cole Brown <code@brown.dev>
2022-01-23 12:43:44 -0800

    Fixed vault init script.

--- os/etc/vault.d/systemd/init.sh
+++ os/etc/vault.d/systemd/init.sh
@@ -7,9 +7,43 @@
 #                with that exit code
 set -euo pipefail
 
+# ------------------------------------------------------------------------------
+# Debug Funcs
+# ------------------------------------------------------------------------------
+
+VAULT_INIT_DEBUG=false
+
+debug_echo() {
+    if $VAULT_INIT_DEBUG; then
+        echo "$@"
+    fi
+}
+
+
 #-------------------------------------------------------------------------------
 # Vault Service Initialization Script
 #-------------------------------------------------------------------------------
+
+# ------------------------------
+# Wait for Vault to finish starting.
+# ------------------------------
+sleep 5s
+
+
+# ------------------------------
+# Fix ~gpg~ Error:
+# ------------------------------
+#   "gpg: can't connect to the agent: Read-only file system"
+
+# Create a temp dir for GPG and delete it on script's exit.
+export GNUPGHOME=$(mktemp -d)
+cp --recursive ~/.gnupg/* "$GNUPGHOME"
+trap "rm -rf '$GNUPGHOME'" EXIT
+
+
+# ------------------------------
+# Unseal Vault.
+# ------------------------------
 
 KEYS=(
     "$RASPI_VAULT_KEY_0"
@@ -17,11 +51,41 @@ KEYS=(
     "$RASPI_VAULT_KEY_2"
 )
 
-# Will delaying a bit make this work?
-sleep 5s
-for key in "${KEYS[@]}"; do
-    vault operator unseal $key
+for encrypted_key in "${KEYS[@]}"; do
+    debug_echo ""
+    debug_echo "│ Encrypted:"
+    debug_echo "│   $encrypted_key"
+    debug_echo "├───────────────────────────────────────────"
+
+    # Decrypt the unseal key.
+    debug_echo "│   Decrypted:"
+    decrypted_key=$(echo "$encrypted_key" | base64 --decode | gpg -dq)
+    if [[ $? -eq 0 ]]; then
+        debug_echo "│     [SUCCESS]"
+    else
+        debug_echo "│     [FAILURE]"
+    fi
+
+    # Apply the unseal key.
+    debug_echo "│   ┌─────────────────┐"
+    debug_echo "│   ├──Vault──Unseal──┤"
+    vault operator unseal $decrypted_key
+    if [[ $? -eq 0 ]]; then
+        debug_echo "│   └────┤SUCCESS├────┘"
+    else
+        debug_echo "│   └────┤FAILURE├────┘"
+    fi
+    debug_echo "└──"
 done
 
-# sealed=$(vault status | grep "Sealed")
-# echo "Vault $sealed"
+
+# ------------------------------
+# Output something for checking in the systemd logs?
+# ------------------------------
+# Find "Sealed       <bool>", trim down.
+sealed=$(vault status | grep "Sealed" | tr -s ' ' | sed 's/Sealed/Sealed:/')
+if [[ "$sealed" == *true ]]; then
+    echo -e "[init:FAILURE] Vault $sealed"
+else
+    echo -e "[init:SUCCESS] Vault $sealed"
+fi
